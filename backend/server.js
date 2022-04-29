cc65858a20790b68ac9445ee8e9ce06dcda4788d
Omar Leiva <oleivahn@gmail.com>
2022-04-29 01:41:33 -0400

    First route created

--- backend/server.js
+++ backend/server.js
@@ -5,4 +5,8 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.get("/api/firstRoute", (req, res) => {
+  res.send("First route officially created")
+})
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
