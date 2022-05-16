e6e8d6dabb5dc9c3da8250496894d6481739e03f
Omar Leiva <oleivahn@gmail.com>
2022-05-16 02:12:16 -0400

    Saved

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,12 @@ const app = express();
 
 // First route example
 app.get("/api/firstRoute", (req, res) => {
-  res.send("First route officially created")
-})
+  res.send("First route officially created");
+});
 
 app.get("/api/goals", (req, res) => {
-  res.json({message: "Get goals"})
-})
+  res.json({ message: "Get goals" });
+});
 
 // TODO: Extract the routes into their own folder on the backend. https://www.youtube.com/watch?v=-0exw-9YJBo (15:50)
 
