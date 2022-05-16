ab341b0149d59546a6c5eaaae7e8abcdccdc77b9
Omar Leiva <oleivahn@gmail.com>
2022-05-16 03:50:56 -0400

    Stopped here

--- backend/server.js
+++ backend/server.js
@@ -13,3 +13,5 @@ app.use("/api/goals", require("./routes/goalRoutes"));
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
 // TODO: Extract the routes into their own folder on the backend. https://www.youtube.com/watch?v=-0exw-9YJBo (26:36 )
+
+// To run this app, run "npm run server"... then on Postman, hit "http://localhost:3000/api/goals"
